7c26ad06ff73561293c073f1ea891e691c6996f0
Vadik <sabalayev1@gmail.com>
2023-02-13 20:40:55 +0200

    Added media for slider

--- js/script.js
+++ js/script.js
@@ -6,9 +6,16 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptiveHeight: true,
-        autoplay: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" alt="" class="prev1"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg" alt="" class="next"></img></button>'
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="" class="prev1"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="" class="next"></img></button>',
+        responsive: [{
+            breakpoint: 900,
+            settings: {
+                arrows: false,
+                dots: true
+            },
+        }]
 
     });
 });
