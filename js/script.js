7adfc16cf0b08b0494ab71f517450ac3ce1ecd06
Vadik <sabalayev1@gmail.com>
2023-02-14 20:15:39 +0200

    Added active class without js script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     $('.slider').slick({
-        dots: false,
+        dots: true,
         infinite: true,
         speed: 1200,
         slidesToShow: 1,
         slidesToScroll: 1,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         autoplay: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="" class="prev1"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="" class="next"></img></button>',
