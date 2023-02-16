4eb78b2bc4ac9d2711a40d90aaead9b63c51fbd4
Vadik <sabalayev1@gmail.com>
2023-02-16 23:07:52 +0200

    Added @media for tabs + feedback sections

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptiveHeight: false,
-        autoplay: false,
+        autoplay: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="" class="prev1"></img></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="" class="next"></img></button>',
         responsive: [{
