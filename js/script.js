a4d39c97b135c8883bcd28a7859ee638b8f18035
Vadik <sabalayev1@gmail.com>
2023-02-16 17:41:08 +0200

    Added feedback section + updated tabs section

--- js/script.js
+++ js/script.js
@@ -18,4 +18,24 @@ $(document).ready(function () {
         }]
 
     });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__main').eq(i).toggleClass('catalog-item__main_active');
+                $('.catalog-item__second').eq(i).toggleClass('catalog-item__second_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+
+
 });
